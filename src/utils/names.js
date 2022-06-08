f14029b0717f951c16ad84f3eab65c02be7465d1
monyverse <geomallapp@gmail.com>
2022-06-08 06:21:34 +0000

    chain4change

--- src/utils/names.js
+++ src/utils/names.js
@@ -4,15 +4,8 @@ export const headerDetails = {
   name: 'Join the Hack2Earn Metaverse',
   title: 'Find your top Job | Recruit the best talent',
   imageAlt: 'The Graph NFT Logo',
-  href: 'https://',
+  href: 'https://chain4change.xyz',
   logo: companyLogo
-};
-
-export const authorDetails = {
-  name: 'Chain4Change',
-  href: 'https://chain4change.',
-  imageAlt: 'Twitter Logo',
-  logo: twitterLogo
 };
 
 export const mainDetails = {
